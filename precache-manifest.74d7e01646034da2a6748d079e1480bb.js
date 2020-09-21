18facbf1816d175b17327ab78660d7457c3e6d4c
Seven <jaseveen@gmail.com>
2020-09-21 23:36:54 +0200

    Updates

---
+++ precache-manifest.74d7e01646034da2a6748d079e1480bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7569148ae489983a14bce5d8b9db624",
+    "url": "/dziekonskik/Pomodoro-Deluxe/index.html"
+  },
+  {
+    "revision": "f62e0a54404333ff0056",
+    "url": "/dziekonskik/Pomodoro-Deluxe/static/css/2.a131f3de.chunk.css"
+  },
+  {
+    "revision": "aef84a30c43bd1eb5319",
+    "url": "/dziekonskik/Pomodoro-Deluxe/static/css/main.d9283525.chunk.css"
+  },
+  {
+    "revision": "f62e0a54404333ff0056",
+    "url": "/dziekonskik/Pomodoro-Deluxe/static/js/2.e19ac9d1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dziekonskik/Pomodoro-Deluxe/static/js/2.e19ac9d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aef84a30c43bd1eb5319",
+    "url": "/dziekonskik/Pomodoro-Deluxe/static/js/main.d72fb12c.chunk.js"
+  },
+  {
+    "revision": "9c3ba1b60f397280e174",
+    "url": "/dziekonskik/Pomodoro-Deluxe/static/js/runtime-main.2301fca6.js"
+  },
+  {
+    "revision": "dd0f186cb2a060b98c37a7306991b656",
+    "url": "/dziekonskik/Pomodoro-Deluxe/static/media/dot.dd0f186c.svg"
+  }
+]);
