14a72fa9a80a55e2e8b20735322846fa64489c77
Seven <jaseveen@gmail.com>
2020-09-28 16:12:26 +0200

    add refactors: delete MainView&HistoryView, moved QuickStartPanel&HistoryPanel to App.js, App-Root View

--- src/views/RootView/App.js
+++ src/views/RootView/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import MainView from '../MainView/MainView';
-import HistoryView from '../HistoryView/HistoryView';
+import HistoryPanel from '../../components/HistoryPanel/HistoryPanel';
+import QuickStartPanel from '../../components/QuickStartPanel/QuickStartPanel';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import styles from './App.module.scss';
@@ -8,17 +8,17 @@ import styles from './App.module.scss';
 function App() {
   return (
     <React.StrictMode>
-      <div className={`App ${styles.bgColor}`}>
+      <div className={`App ${styles.bgColor} vh-100`}>
         <Container className="py-5">
           <Row className="h-100">
             <Col sm={12} md={{ order: 2 }} className="col-md-9 shadow">
-              <MainView />
+              <QuickStartPanel />
             </Col>
             <Col
               sm={12}
               className="col-md-3 border border-danger rounded shadow"
             >
-              <HistoryView />
+              <HistoryPanel />
             </Col>
           </Row>
         </Container>
--- src/views/StatsView/StatsView.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class StatsView extends React.Component {
-  render() {
-    return <div>.</div>;
-  }
-}
-
-export default StatsView;
