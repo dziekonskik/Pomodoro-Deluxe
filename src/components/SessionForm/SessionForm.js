150929879753bf49dfa9ce6e0c7dbea7e3f6bdb6
Seven <jaseveen@gmail.com>
2020-10-10 18:07:38 +0200

    add stylles for SessionForm list and list item

--- src/components/SessionForm/SessionForm.js
+++ src/components/SessionForm/SessionForm.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import { Container, Row, Col, ListGroup } from 'react-bootstrap';
 import TimeUnit from '../TimeUnit/TimeUnit';
 import UtilityButton from '../PomodoroButtons/UtilityButton';
+import SessionListItem from './SessionListItem';
 import styles from './SessionForm.module.scss';
 
 export class SessionForm extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      workMinutes: 0,
+      restMinutes: 0,
+    };
   }
 
   render() {
@@ -36,12 +40,22 @@ export class SessionForm extends Component {
           </UtilityButton>
         </Row>
         <Row className="d-flex justify-content-center text-center">
-          <ListGroup>
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-          </ListGroup>
+          <Col className="overflow-auto" style={{ 'max-height': '240px' }}>
+            <ListGroup>
+              <SessionListItem />
+              <SessionListItem />
+              <SessionListItem />
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+              <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+            </ListGroup>
+          </Col>
+        </Row>
+        <Row className="d-flex justify-content-around align-items-center my-3">
+          <UtilityButton size={'lg'} variant={'outline-warning'}>
+            Complete Session
+          </UtilityButton>
         </Row>
       </Container>
     );
