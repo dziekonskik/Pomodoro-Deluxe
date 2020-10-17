ff6fa3d0a0e983fc2c4b8d8c32ee02e75ffe5520
Seven <jaseveen@gmail.com>
2020-10-17 17:02:32 +0200

    add sessionCountCard styles

--- src/components/QuickStartPanel/SessionCountCard.js
+++ src/components/QuickStartPanel/SessionCountCard.js
@@ -3,14 +3,14 @@ import { Card } from 'react-bootstrap';
 
 const SessionCountCard = ({ sessionsTotal, currentSession, icon }) => {
   return (
-    <Card bg="warning" text="light">
+    <Card bg="light" border="dark" text="dark">
       <Card.Body>
         <Card.Text className="d-flex justify-content-around align-items-center">
           <div>
             <span className="h3 mx-3"> {currentSession}</span> /{' '}
             <span className="h3 mx-3">{sessionsTotal}</span>{' '}
           </div>
-          <span className="h1 text-danger">{icon}</span>
+          <span className="h1 text-dark">{icon}</span>
         </Card.Text>
       </Card.Body>
     </Card>
