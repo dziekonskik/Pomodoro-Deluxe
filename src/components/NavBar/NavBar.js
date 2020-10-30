ac9d8f94774948dd2eea9932349852801ad4bd21
Seven <jaseveen@gmail.com>
2020-10-30 20:26:18 +0100

    change navbar to function

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,51 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-export class NavBar extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      activeTab: '',
-    };
-  }
-
-  setActiveTab = (selectedTab) => {
-    this.setState({ activeTab: selectedTab });
-  };
-
-  render() {
-    const { activeTab } = this.state;
-    return (
-      <Nav
-        onSelect={(eventKey) => this.setActiveTab(eventKey)}
-        variant="tabs"
-        defaultActiveKey="/"
-      >
-        <Nav.Item>
-          <Nav.Link eventKey="/" as={Link} to="/">
-            Home
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="/session" as={Link} to="/session">
-            Set Sessions
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="/stats" as={Link} to="/stats">
-            Stats
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="/hydrapp" as={Link} to="/hydrapp">
-            Hydrapp!
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
-    );
-  }
-}
+const NavBar = () => (
+  <Nav variant="tabs" defaultActiveKey="/">
+    <Nav.Item>
+      <Nav.Link eventKey="/" as={Link} to="/">
+        Home
+      </Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="/session" as={Link} to="/session">
+        Set Sessions
+      </Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="/stats" as={Link} to="/stats">
+        Stats
+      </Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="/hydrapp" as={Link} to="/hydrapp">
+        Hydrapp!
+      </Nav.Link>
+    </Nav.Item>
+  </Nav>
+);
 
 export default NavBar;
